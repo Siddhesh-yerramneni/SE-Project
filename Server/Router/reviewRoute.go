2ba04524f59528fc09772522969d9677ad33b963
Siddhesh-yerramneni <siddhesh.yerramneni@gmail.com>
2025-03-26 20:51:47 -0400

    Created review Route

---
+++ Server/Router/reviewRoute.go
@@ -0,0 +1,10 @@
+package router
+
+import (
+	"github.com/gofiber/fiber/v2"
+	controller "github.com/siddhesh-yerramneni/SE-Project/Server/Controller"
+)
+
+func SetupReviewRoutes(app *fiber.App) {
+	app.Post("/addReview", controller.AddReview)
+}
