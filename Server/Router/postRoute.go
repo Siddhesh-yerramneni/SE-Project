635b260326c7d248ce11caea26ae90a1f0f3f432
Siddhesh-yerramneni <siddhesh.yerramneni@gmail.com>
2025-04-18 18:43:15 -0400

    Added route for getting posts based on author id

--- Server/Router/postRoute.go
+++ Server/Router/postRoute.go
@@ -9,5 +9,6 @@ func SetupPostRoutes(app *fiber.App) {
 	app.Post("/createPost", controller.CreatePost)
 	app.Delete("/deletePost/:id", controller.DeletePost)
 	app.Get("/getPosts", controller.GetAllPosts)
+	app.Get("/getPosts/author/:authorID", controller.GetPostsByAuthorID)
 
 }
