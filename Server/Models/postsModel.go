7cf540d4e88ff0ca72bea5fcbe99d6ca742d2cf4
Siddhesh-yerramneni <siddhesh.yerramneni@gmail.com>
2025-04-18 18:21:39 -0400

    Corrected the mistake of taking time as string in model

--- Server/Models/postsModel.go
+++ Server/Models/postsModel.go
@@ -1,10 +1,12 @@
 package model
 
+import "time"
+
 type Post struct {
-	ID        uint   `json:"id" gorm:"primaryKey;autoIncrement"`
-	AuthorID  uint   `json:"author_id" gorm:"not null"`                                      // FK to User
-	Title     string `json:"title" gorm:"not null;size:255"`                                 // Post title
-	Content   string `json:"content" gorm:"not null;type:text"`                              // Full content
-	CreatedAt string `json:"created_at" gorm:"autoCreateTime"`                               // Timestamp
-	Author    *User  `json:"author" gorm:"foreignKey:AuthorID;constraint:OnDelete:CASCADE;"` // Associated user
+	ID        uint      `json:"id" gorm:"primaryKey;autoIncrement"`
+	AuthorID  uint      `json:"author_id" gorm:"not null"`                                      // FK to User
+	Title     string    `json:"title" gorm:"not null;size:255"`                                 // Post title
+	Content   string    `json:"content" gorm:"not null;type:text"`                              // Full content
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`                               // Timestamp
+	Author    *User     `json:"author" gorm:"foreignKey:AuthorID;constraint:OnDelete:CASCADE;"` // Associated user
 }
